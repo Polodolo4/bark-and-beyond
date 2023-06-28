8e6d037c0a660cccde854e8bda3c1e1fdfe016f3
Polodolo4 <86023627+Polodolo4@users.noreply.github.com>
2023-06-28 18:22:55 -0400

    changed from swiper to snap-carousel

--- PupPlanner/src/Dashboard/components/Navigation.js
+++ PupPlanner/src/Dashboard/components/Navigation.js
@@ -1,48 +1,58 @@
 import React from "react";
 import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
-import Swiper from "react-native-swiper";
+import Carousel from "react-native-snap-carousel-v4";
 
 const { width } = Dimensions.get("window");
-const slideWidth = width * 0.45;
+const slideWidth = width * 0.5;
+
+const slides = [
+  {
+    id: 1,
+    backgroundColor: "#B8DFA9",
+    imageSource: require("../assets/services.png"),
+    title: "Services",
+  },
+  {
+    id: 2,
+    backgroundColor: "#BAE7FF",
+    imageSource: require("../assets/learning.png"),
+    title: "Learning",
+  },
+  {
+    id: 3,
+    backgroundColor: "#FFD773",
+    imageSource: require("../assets/community.png"),
+    title: "Community",
+  },
+];
 
 const Navigation = () => {
+  const renderItem = ({ item }) => {
+    return (
+      <View style={[styles.slide, { backgroundColor: item.backgroundColor }]}>
+        <Image
+          source={item.imageSource}
+          style={styles.image}
+        />
+        <Text style={styles.text}>{item.title}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Navigation</Text>
       <View style={styles.content}>
-        <Swiper
-          style={styles.wrapper}
-          showsPagination={false}
-          showsButtons={false}
-        >
-          <View style={[styles.slide, { backgroundColor: "#B8DFA9" }]}>
-            <View style={styles.slideContent}>
-              <Image
-                source={require("../assets/services.png")}
-                style={styles.image}
-              />
-              <Text style={styles.text}>Services</Text>
-            </View>
-          </View>
-          <View style={[styles.slide, { backgroundColor: "#BAE7FF" }]}>
-            <View style={styles.slideContent}>
-              <Image
-                source={require("../assets/learning.png")}
-                style={styles.image}
-              />
-              <Text style={styles.text}>Learning</Text>
-            </View>
-          </View>
-          <View style={[styles.slide, { backgroundColor: "#FFD773" }]}>
-            <View style={styles.slideContent}>
-              <Image
-                source={require("../assets/community.png")}
-                style={styles.image}
-              />
-              <Text style={styles.text}>Community</Text>
-            </View>
-          </View>
-        </Swiper>
+        <Carousel
+          data={slides}
+          renderItem={renderItem}
+          sliderWidth={width}
+          itemWidth={slideWidth}
+          inactiveSlideScale={0.8}
+          inactiveSlideOpacity={0.6}
+          loop={true}
+          loopClonesPerSide={slides.length}
+        />
       </View>
     </View>
   );
@@ -62,17 +72,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     paddingTop: 24,
   },
-  wrapper: {},
   slide: {
     width: slideWidth,
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 25,
-  },
-  slideContent: {
-    height: 230,
-    justifyContent: "center",
-    alignItems: "center",
+    borderRadius: 50,
   },
   image: {
     height: 142,
