ccff13b99a4116feae9b3df319508060bc48af3b
Polodolo4 <86023627+Polodolo4@users.noreply.github.com>
2023-08-13 14:42:30 -0400

    AddToPN initial commit after starting from scratch

--- PupPlanner/src/Dashboard/components/AddToPN.js
+++ PupPlanner/src/Dashboard/components/AddToPN.js
@@ -1,15 +1,21 @@
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
   View,
+  TextInput,
+  SafeAreaView,
   TouchableOpacity,
   Image,
-  TextInput,
 } from "react-native";
-import React, { useState } from "react";
-import { useNavigation } from "@react-navigation/native";
 
 import * as ImagePicker from "expo-image-picker";
+import { useNavigation } from "@react-navigation/native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+import { firebase } from "../../../Firebase/firebase";
+import "firebase/storage";
+import "firebase/firestore";
 
 const InputField = ({
   value,
@@ -33,14 +39,34 @@ const InputField = ({
 
 const AddToPN = () => {
   const [name, setName] = useState("");
-  const [nickname, setNickame] = useState("");
+  const [nickname, setNickname] = useState("");
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
+  const [image, setImage] = useState(null);
 
   const navigation = useNavigation();
 
-  const handleCancel = () => {
-    navigation.goBack();
+  const pickImage = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (status !== "granted") {
+      alert("Sorry, we need camera roll permissions to make this work!");
+      return;
+    }
+
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets);
+      console.log("Image has been set.");
+    } else {
+      console.log("something went wrong with the damn image");
+    }
   };
 
   const handleNameChange = (value) => {
@@ -59,169 +85,144 @@ const AddToPN = () => {
     setEmail(value);
   };
 
-  const pickImage = async () => {
-    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-    if (status !== "granted") {
-      alert("Sorry, we need camera roll permissions to make this work!");
-      return;
-    }
-
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      setImage(result.uri);
-    }
-  };
-
-  const uploadImage = async () => {
-    try {
-      if (!image) return null;
-
-      const response = await fetch(image);
-
-      const blob = await response.blob();
-
-      const timestamp = Date.now();
-      const filename = `Image${timestamp};`;
-      const ref = firebase.storage().ref().child(`dogPictures/${filename}`);
-
-      const snapshot = await ref.put(blob);
-
-      const downloadUrl = await snapshot.ref.getDownloadURL();
-
-      return downloadUrl;
-    } catch (error) {
-      console.log("Error in uploadImage function: ", error);
-      throw error;
-    }
-  };
-
   return (
-    <View style={styles.container}>
+    <KeyboardAwareScrollView>
       <View style={styles.greenBackground}>
-        <View style={styles.rowContainer}>
-          <TouchableOpacity onPress={handleCancel}>
-            <Text style={styles.cancel}>Cancel</Text>
-          </TouchableOpacity>
-
-          <View style={styles.addToContainer}>
+        <View style={styles.topButtons}>
+          <View style={styles.buttonsContainer}>
             <TouchableOpacity
-              onPress={() => console.log("Add to network pressed")}
+              style={styles.cancelContainer}
+              onPress={() => navigation.goBack()}
             >
-              <Text style={styles.addTo}>Add to your network</Text>
+              <Text style={styles.cancel}>Cancel</Text>
             </TouchableOpacity>
+
+            <View style={styles.addContainer}>
+              <Text style={styles.addToNetwork}>Add to your network</Text>
+            </View>
           </View>
         </View>
+      </View>
 
-        <TouchableOpacity
-          style={styles.addPhotoContainer}
-          onPress={pickImage}
-        >
+      <TouchableOpacity
+        style={styles.photoButton}
+        onPress={pickImage}
+      >
+        {image ? (
           <Image
-            style={styles.addPhoto}
+            source={{ assets: image }}
+            style={{ width: "100%", height: "100%", borderRadius: 100 }}
+          />
+        ) : (
+          <Image
+            style={styles.photoButton}
             source={require("../assets/add-photo.png")}
           />
-        </TouchableOpacity>
-      </View>
+        )}
+      </TouchableOpacity>
 
-      <View style={styles.inputFieldsContainer}>
-        <InputField
-          value={name}
-          onChangeText={handleNameChange}
-          label="Friend's name"
-          placeholder="Enter name"
-        />
-        <InputField
-          value={nickname}
-          onChangeText={handleNicknameChange}
-          label="Nicname (Optional)"
-        />
-        <InputField
-          value={phone}
-          onChangeText={handlePhoneChange}
-          label="Pup's name"
-          placeholder="Enter pup's name"
-        />
-        <InputField
-          value={email}
-          onChangeText={handleEmailChange}
-          label="Pup's name"
-          placeholder="Enter pup's name"
-        />
-      </View>
-    </View>
+      <InputField
+        value={name}
+        onChangeText={handleNameChange}
+        label="Friend's name"
+        placeholder="Enter name"
+      />
+
+      <InputField
+        value={nickname}
+        onChangeText={handleNicknameChange}
+        label="Nickname (optional)"
+      />
+
+      <InputField
+        value={phone}
+        onChangeText={handlePhoneChange}
+        label="Phone number"
+        placeholder="Enter 10 digit number"
+      />
+      <InputField
+        value={email}
+        onChangeText={handleEmailChange}
+        label="Email"
+        placeholder="john.doe@email.com"
+      />
+
+      <TouchableOpacity
+        style={styles.continueButton}
+        //  onPress={createAndMoveScreens}
+      >
+        <Text style={styles.continueText}>Continue</Text>
+      </TouchableOpacity>
+    </KeyboardAwareScrollView>
   );
 };
 
 export default AddToPN;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   greenBackground: {
-    height: 98,
     backgroundColor: "#B8DFA9",
+    height: 98,
   },
-  rowContainer: {
+  buttonsContainer: {
     flexDirection: "row",
-    alignItems: "center",
+    marginHorizontal: 24,
     marginTop: 60,
-    marginLeft: 24,
   },
   cancel: {
     fontSize: 16,
     fontWeight: "600",
     color: "#323841",
   },
-  addToContainer: {
-    flex: 0.76,
+  addContainer: {
+    flex: 0.85,
     justifyContent: "center",
     alignItems: "center",
   },
-  addTo: {
-    fontSize: 16,
-    fontWeight: "700",
+  addToNetwork: {
     color: "#000",
+    fontSize: 16,
+    fontWeight: 700,
   },
-  addPhotoContainer: {
-    flex: 1,
-    // justifyContent: "center",
-    alignItems: "center",
-    marginTop: 32,
-    marginBottom: 24,
-  },
-  addPhoto: {},
-  inputFieldsContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 24,
+  photoButton: {
+    marginTop: 30,
+    marginBottom: 8,
+    alignSelf: "center",
   },
   inputContainer: {
-    width: "90%",
+    marginLeft: 29,
+    marginRight: 19,
     marginTop: 16,
   },
   inputLabel: {
     fontSize: 16,
-    marginBottom: 5,
+    marginBottom: 6,
     marginTop: 16,
     fontWeight: "bold",
   },
   input: {
     height: 48,
-    borderColor: "#333",
     borderWidth: 1,
     borderRadius: 10,
     padding: 12,
     backgroundColor: "#FFF",
     fontSize: 16,
     fontWeight: "bold",
+  },
+  continueButton: {
+    //  display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    width: "45%",
+    height: 48,
+    backgroundColor: "#323841",
+    borderRadius: 30,
+    marginTop: 32,
+  },
+  continueText: {
+    color: "#FFF",
+    fontSize: 16,
+    fontWeight: 600,
   },
 });
